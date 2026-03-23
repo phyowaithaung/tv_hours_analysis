2cf520a405193942f1455a551535198a9f550740
phyowaithaung <phyowaithaung.md@gmail.com>
2026-03-23 23:14:13 +0700

    filtered to age 30 and above

--- main.R
+++ main.R
@@ -1,6 +1,10 @@
+install.packages("tidyverse")
 library(tidyverse)
 library(here)
 
 tv_hours_table <-  gss_cat %>% 
+  filter(age >=30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm= T))
+
+write_csv(tv_hours_table,here("tv_hours_by_marital.csv"))
