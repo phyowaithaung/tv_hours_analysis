f4d702508e3a7460dfbb8ff15ded0017f0c75415
phyowaithaung <phyowaithaung.md@gmail.com>
2026-03-24 13:25:28 +0700

    shorten names

--- main.R
+++ main.R
@@ -2,9 +2,9 @@ install.packages("tidyverse")
 library(tidyverse)
 library(here)
 
-tv_hours_table <-  gss_cat %>% 
+tv_hours_tab <-  gss_cat %>% 
   filter(age <30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm= T))
 
-write_csv(tv_hours_table,here("tv_hours_by_marital.csv"))
+write_csv(tv_hours_tab,here("tv_hours_by_marital.csv"))
